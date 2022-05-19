cd09ffba262f242a76dbbdc03bf266f82f04a752
1sol <17gksthf@gmail.com>
2022-05-19 12:07:22 +0900

    관계연산자, 논리연산자

--- SeSAC/DAY 13/js3.js
+++ SeSAC/DAY 13/js3.js
@@ -18,3 +18,27 @@ console.log(num3); // false : 타입은 다르지만 값은 다르므로 false�
 
 let num4 = num1 !== num2;
 console.log(num4); // true : 타입과 값이 모두 같지 않으므로 true를 반환함
+
+console.clear()
+
+let v1=10 , v2=20, v3=30, v4=40, result; // result는 값이 할당안되고 변수 선언만 된 상태(undefined)
+result = !(v1>v2); // a가 b보다 크지않다.
+console.log(result); // true
+
+result = !!(v1>v2); 
+console.log(result); // false (true값의 부정)
+
+
+// 논리 연산자
+result = v1>v2 && v2<=v3;
+console.log(result); //false
+
+result = v1<v2 && v2<=v3;
+console.log(result); // true
+
+result = v1>v2 || v2<=v3 || v3<=v4;
+console.log(result); // true
+
+//삼항연산자, 조건부연산자
+result = v1>v2 ? 'A' : 'B'; // v1이 v2보다 크면 A를 출력, 작으면 B를 출력함
+console.log(result); // B
