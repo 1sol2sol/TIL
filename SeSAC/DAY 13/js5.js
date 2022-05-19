1becb3458acdefa7f74117b897f7de739339f253
1sol <17gksthf@gmail.com>
2022-05-19 12:06:20 +0900

    조건문

---
+++ SeSAC/DAY 13/js5.js
@@ -0,0 +1,17 @@
+// 조건문
+/*
+1. if() 문장 
+ 
+2. if(){
+     문장
+ }
+*/
+
+let userId = '1sol';
+
+if(userId === '1so2'){
+    // console.log(userId+'님 감사감사해용');
+    console.log(`${userId}님 감사해용`);
+} else{
+    console.log('다시 로그인해주세요');
+}
