c34ca3e6df6687799898827b2b11ea097904bef6
1sol <17gksthf@gmail.com>
2022-05-26 21:06:58 +0900

    priority

---
+++ JAVASCRIPT/2.operator/7.priority.js
@@ -0,0 +1,11 @@
+// 연산자 우선순위
+
+let a = 2;
+let b = 3;
+let result = a + b * 4; // 14
+console.log(result);
+result = a++ + b * 4; // 14
+console.log(result);
+console.log(a); //3
+
+// 우선시 하고 싶은것들에 () 사용 
