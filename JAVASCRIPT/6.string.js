ba5677d4886267d77a28a01a2304cef306148be5
1sol <17gksthf@gmail.com>
2022-05-31 21:24:22 +0900

    string

---
+++ JAVASCRIPT/6.string.js
@@ -0,0 +1,9 @@
+// MDN String: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+const textObj = new String("Hello World!");
+const text = "Hello World!";
+
+console.log(textObj);
+console.log(text);
+
+console.log(text[0]);
