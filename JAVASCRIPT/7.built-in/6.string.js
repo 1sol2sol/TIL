6147d2378dabb7b88186d4582c00e7446e212372
1sol2sol <17gksthf@gmail.com>
2022-06-03 16:13:09 +0900

    string

---
+++ JAVASCRIPT/7.built-in/6.string.js
@@ -0,0 +1,38 @@
+// MDN String: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+
+const textObj = new String("Hello World!");
+const text = "Hello World!";
+
+console.log(textObj);
+console.log(text);
+
+console.log(text[0]);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text.charAt(0));
+console.log(text.charAt(1));
+console.log(text.charAt(2));
+
+console.log(text.indexOf('l')); // 2
+console.log(text.lastIndexOf('l')); // 9
+
+console.log(text.includes('tx'));
+console.log(text.includes('h'));
+console.log(text.includes('H'));
+
+console.log(text.startsWith('He'));
+console.log(text.endsWith('!'));
+
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+console.log(text.substring(0,2)); // He 
+console.log(text.slice(2)); // llo World!
+console.log(text.slice(-2)); // d!
+
+const space = '              space       ';
+console.log(space.trim()); // space
+
+const longText = 'Get to the, point';
+console.log(longText.split(' '));
+console.log(longText.split(', ', 2));
