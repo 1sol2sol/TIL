357e08f26a6e2260524a10246e358cd33e22b9ef
1sol2sol <17gksthf@gmail.com>
2022-06-03 16:12:28 +0900

    del

--- JAVASCRIPT/6.string.js
+++
@@ -1,9 +0,0 @@
-// MDN String: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-
-const textObj = new String("Hello World!");
-const text = "Hello World!";
-
-console.log(textObj);
-console.log(text);
-
-console.log(text[0]);
