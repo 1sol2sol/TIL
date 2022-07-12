7f7e656a17be7109bbdb905b872f331a8f4c71c2
1sol2sol <17gksthf@gmail.com>
2022-07-13 01:01:48 +0900

    입출력(나누기)

---
+++ Algorithm/Baekjoon_JS/1008.js
@@ -0,0 +1,11 @@
+// 두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+// 입력: 1 3
+// 출력: 0.33333333333333333333333333333333
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a/b);
