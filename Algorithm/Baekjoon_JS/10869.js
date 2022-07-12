d2378d6f0d9d24fd533834ef0eaac886786e1155
1sol2sol <17gksthf@gmail.com>
2022-07-13 01:03:05 +0900

    입출력(사칙연산)

---
+++ Algorithm/Baekjoon_JS/10869.js
@@ -0,0 +1,23 @@
+// 두 자연수 A와 B가 주어진다. 이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+// 입력: 7 3
+/*
+출력:  10
+      4
+      21
+      2
+      1
+*/
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let input = fs.readFileSync(filePath).toString().split(' ');
+
+const a = parseInt(input[0]);
+const b = parseInt(input[1]);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(Math.floor(a/b));
+console.log(a%b);
+
