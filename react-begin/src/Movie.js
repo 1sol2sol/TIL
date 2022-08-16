c5102642923168e8fa239fbbf0e9b1b8ed44a170
1sol2sol <17gksthf@gmail.com>
2022-08-16 17:15:59 +0900

    Movie

---
+++ react-begin/src/Movie.js
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+
+function Movie(){
+ return (
+  <Router>
+    <Routes>
+      <Route path="/movie/:id" element={<Detail/>}></Route> 
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home/>}></Route>
+    </Routes>
+  </Router>
+ );
+}
+
+export default Movie;
