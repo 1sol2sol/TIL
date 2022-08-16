a88b14dbb957622c9a5cab62fb71f35cf4b98473
1sol2sol <17gksthf@gmail.com>
2022-08-16 17:16:07 +0900

    index

---
+++ react-begin/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Movie from './Movie';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  // <React.StrictMode>
+    <Movie />
+  // </React.StrictMode>
+);
+
