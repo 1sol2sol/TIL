1f002541491fdda59cebfe5753b7bfa93a9afc4c
1sol2sol <17gksthf@gmail.com>
2022-08-21 23:18:52 +0900

    darkmode

--- react-master/src/App.js
+++ react-master/src/App.js
@@ -1,27 +1,54 @@
-import styled from "styled-components"
+import styled, {keyframes} from "styled-components"
 
-const Father = styled.div`
+const Title = styled.h1`
+  color: ${props => props.theme.textColor}
+`
+
+const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.backgroundColor}
 `
+const animation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50%{
+    transform: rotate(360deg);
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`
+const Emoji = styled.span`
+  font-size: 37px;
+`
+
 const Box = styled.div`
-  background-color: ${props => props.bgColor};
-  width: 100px;
-  height: 100px;
-`
-const Circle = styled.div`
-  background-color: ${props => props.bgColor};
-  width: 100px;
-  height: 100px;
-  border-radius: 50px;
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation:${animation} 1s linear infinite;
+  ${Emoji}:hover {
+    font-size: 150px;
+  }
+  
 `
 
 function App() {
   return (
-    <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
-    </Father> 
+    <Wrapper>
+      <Title>Hello</Title>
+    </Wrapper>
   )
 }
-
 export default App;
