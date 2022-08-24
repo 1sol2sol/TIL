d0c1ec0dd40c33ed9f74ade2b9015b88cb225bda
1sol2sol <17gksthf@gmail.com>
2022-08-24 16:29:44 +0900

    Add store, reducer , onSubmit func

--- vanilla-redux/src/index.js
+++ vanilla-redux/src/index.js
@@ -1,39 +1,31 @@
-import { createStore } from "redux"; 
+import { createStore } from "redux"
 
+const form = document.querySelector("form")
+const input = document.querySelector("input")
+const ul = document.querySelector("ul")
 
-const add = document.getElementById("add")
-const minus = document.getElementById("minus")
-const number = document.querySelector("span")
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
 
-
-number.innerText = 0;
-
-const ADD = "ADD";
-const MINUS = "MINUS";
-
-// reducer is a function that modify my data
-// action: redux에서 function을 부를때쓰는 두번째 parameter
-const countModifier = (count = 0, action) => {
-  // return값이 나의 application data가 되는것 
+const reducer = (state = [], action) => {
   switch(action.type){
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
-    default:
-      return count;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
+      default:
+        return state; 
   }
 }
 
-// store는 나의 data(state)를 저장하는곳(state는 나의 app에서 바뀌는 data를 말함)
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChage = () => {
-  number.innerText = countStore.getState();
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  store.dispatch({type: ADD_TODO, text: toDo})
 }
 
-// subscribe : store안에 있는 변화들을 알려줌 
-countStore.subscribe(onChage)
-
-add.addEventListener("click",() => countStore.dispatch({type: ADD}))
-minus.addEventListener("click",() => countStore.dispatch({type: MINUS}))
+form.addEventListener("submit", onSubmit)
