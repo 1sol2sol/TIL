0547585ca435ac33666a9a19908ee1047f769b01
1sol2sol <17gksthf@gmail.com>
2022-08-25 03:05:20 +0900

    Home Route
    key: useSelector, useDispatch

---
+++ vanilla-redux/src/routes/Home.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDo } from "../store";
+import ToDo from "../components/ToDo";
+
+function Home(){
+  const [text, setText] = useState("");
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
+  const toDo = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addToDo(text))
+    setText("");
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange}/>
+        <button>Add</button>
+      </form>
+      <ul>{toDo.map(todo => <ToDo {...todo} key={todo.id}/>)}</ul>
+    </>
+  )
+}
+
+
+
+export default Home;
