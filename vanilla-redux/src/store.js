858793b14bb14f7c9c19f64a4e9e2741af604e16
1sol2sol <17gksthf@gmail.com>
2022-09-12 23:47:41 +0900

    refact: createStore -> configureStore

--- vanilla-redux/src/store.js
+++ vanilla-redux/src/store.js
@@ -1,40 +1,57 @@
 import { createStore } from "redux";
-
-const ADD = "ADD"
-const DELETE = "DELETE"
-const TODOS_KEY = "toDos";
-
-export const addToDo = (text) => {
-  return{type:ADD, text}
-}
-
-export const deleteToDo = (id) => {
-  return{type:DELETE, id}
-}
+import { configureStore, createAction, createReducer, createSlice } from "@reduxjs/toolkit";
 
 
-JSON.parse(localStorage.getItem("toDos")) || localStorage.setItem("toDos", JSON.stringify([]));
-const savedToDos = localStorage.getItem(TODOS_KEY);
+// const TODOS_KEY = "toDos";
 
+// export const addToDo = createAction("ADD");
+// export const deleteToDo = createAction("DELETE");
 
-const reducer = (state =JSON.parse(savedToDos), action) => {
-  switch(action.type){
-    case ADD:
-      const addItem = [{text: action.text, id: Date.now()}, ...state];
-      localStorage.setItem(TODOS_KEY, JSON.stringify(addItem));
-      return addItem;
-    case DELETE:
-      const delItem = state.filter(toDo => toDo.id !== action.id);
-      localStorage.setItem(TODOS_KEY, JSON.stringify(delItem));
-      return delItem;
-    default:
-      return state;
+// const reducer = (state =JSON.parse(savedToDos), action) => {
+//   switch(action.type){
+//     case addToDo.type:
+//       console.log(action);
+//       const addItem = [{text: action.payload, id: Date.now()}, ...state];
+//       localStorage.setItem(TODOS_KEY, JSON.stringify(addItem));
+//       return addItem;
+//     case deleteToDo.type:
+//       const delItem = state.filter(toDo => toDo.id !== action.payload);
+//       localStorage.setItem(TODOS_KEY, JSON.stringify(delItem));
+//       return delItem;
+//     default:
+//       return state;
+//   }
+// }
+
+// const reducer = createReducer([],{
+//   [addToDo]: (state,action) => {
+//     state.push({text: action.payload, id: Date.now()})
+//   },
+//   [deleteToDo]:(state,action) =>  state.filter(toDo => toDo.id !== action.payload)
+// })
+
+// JSON.parse(localStorage.getItem("toDos")) || localStorage.setItem("toDos", JSON.stringify([]));
+// const savedToDos = localStorage.getItem(TODOS_KEY);
+
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state,action) => {
+      state.push({text: action.payload, id: Date.now()});
+    },
+    remove: (state,action) => {
+      state.filter(toDo => toDo.id !== action.payload)
+    }
   }
-}
+})
 
-const store = createStore(reducer)
+const store = configureStore({reducer: toDos.reducer})
 
-
+export const{
+  add,
+  remove
+} = toDos.actions
 
 
 export default store;
