f9e8eb726c691e8a40ce0fab892ddb44e8c8874f
1sol2sol <17gksthf@gmail.com>
2022-09-14 19:53:24 +0900

    redirects(), rewrites()

--- nextjs-intro/next.config.js
+++ nextjs-intro/next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects(){
+    return [
+    {
+    source: "/contact/:path*",
+    destination:"/form/:path*",
+    permanent: false, /// 브라우저가 계속 기억해야 하는지를 정함
+    }
+    ]
+    },
+  async rewrites(){
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
