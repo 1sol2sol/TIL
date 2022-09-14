63147e2b7f3cab35b21c9898d347f9686281bdc6
1sol2sol <17gksthf@gmail.com>
2022-09-14 20:20:29 +0900

    Add getServerSideProps() func
    fetching inside getServerSideProps()

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,22 +1,12 @@
 import { useState, useEffect } from "react"
 import Seo from "../components/Seo";
 
-export default function Home(){
-  const [movies, setMovies] = useState();
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await 
-        fetch(
-        `/api/movies`
-        )).json();
-        setMovies(results)
-      })();
-  },[])
+export default function Home({results}){
+
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...</h4>}
-      {movies?.map(movie =>( <div key={movie.id} className="movie" >
+      {results?.map(movie =>( <div key={movie.id} className="movie" >
         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
         <h4>{movie.original_title}</h4>
       </div>))}
@@ -43,4 +33,14 @@ export default function Home(){
       `}</style>
     </div>
   )
+}
+
+export async function getServerSideProps(){// getServerSideProps 이름 변경 불가!
+// 여기에 작성하면 only server에서만 돌아가게 됨 
+  const {results} = await (await fetch(`http://localhost:3000//api/movies`)).json();
+  return {
+    props: {
+      results,
+    }
+  }
 }
