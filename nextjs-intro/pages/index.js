36a6016206fc4935fd737664e22bbafbba6813a2
1sol2sol <17gksthf@gmail.com>
2022-09-14 18:05:57 +0900

    fetching to paint movie title

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,11 +1,26 @@
-import NavBar from "../components/NavBar";
+import { useState, useEffect } from "react"
+import Seo from "../components/Seo";
+
+
+const API_KEY = "1b81a9c70f47931dc69c3bad709be511";
 
 export default function Home(){
+  const [movies, setMovies] = useState();
+  useEffect(() => {
+    (async () => {
+      const { results } = await (await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )).json();
+        setMovies(results)
+      })();
+  },[])
   return (
     <div>
-      <h1 className="active">Hello</h1>
-
-      
+      <Seo title="Home" />
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map(movie =>( <div key={movie.id}>
+        <h4>{movie.original_title}</h4>
+      </div>))}
     </div>
   )
 }
