f3d9abe7620fb9624a3941a9440102c67b0091fd
1sol2sol <17gksthf@gmail.com>
2022-09-15 23:57:51 +0900

    Create Detail page

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,21 +1,42 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState, useEffect } from "react"
 import Seo from "../components/Seo";
 
 export default function Home({results}){
-
+  const router = useRouter();
+  const onClick = (id, title) => {
+    router.push({
+      pathname: `movies/${id}`,
+      query: {
+        title: `${title}`,
+      }
+    }, `movies/${id}`)
+  }
   return (
     <div className="container">
       <Seo title="Home" />
-      {results?.map(movie =>( <div key={movie.id} className="movie" >
-        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
-        <h4>{movie.original_title}</h4>
-      </div>))}
+      {results?.map(movie =>( 
+          <div onClick={() => onClick(movie.id, movie.original_title)} className="movie" key={movie.id} >
+            <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+            <h4>
+              <Link  href={`/movies/${movie.id}`}>
+                <a>{movie.original_title}</a>
+              </Link>
+            </h4>
+          </div>
+        
+      
+      ))}
       <style jsx>{`
         .container {
           display: grid;
           grid-template-columns: 1fr 1fr;
           padding: 20px;
           gap: 20px;
+        }
+        .movie {
+          cursor: pointer;
         }
         .movie img {
           max-width: 100%;
