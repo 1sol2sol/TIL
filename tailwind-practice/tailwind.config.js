36c45e23d18fccd82b39df5dc3d6df5ecb4f97e8
1sol2sol <17gksthf@gmail.com>
2022-09-28 14:35:48 +0900

    Set tailwind css

---
+++ tailwind-practice/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}", // pages/모든폴더/모든파일/{확장자 나열}
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
