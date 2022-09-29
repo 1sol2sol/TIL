5f3051028a8c11e75b4ddf3da9107dab68e8549a
1sol2sol <17gksthf@gmail.com>
2022-09-29 16:42:53 +0900

    name input 추가

--- tailwind-ui/pages/items/upload.tsx
+++ tailwind-ui/pages/items/upload.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
+    <div className="px-4 space-y-5  py-16">
       <div>
         <label className="w-full cursor-pointer flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md text-gray-600 hover:text-orange-500 hover:border-orange-500">
           <svg
@@ -23,23 +23,56 @@ const Upload: NextPage = () => {
           <input className="hidden" type="file" />
         </label>
       </div>
-      <div className="my-5">
-        <label htmlFor="price" className="mb-1 block text-sm font-medium text-gray-700">Price</label>
+      <div>
+        <label
+          className="mb-1 block text-sm font-medium text-gray-700"
+          htmlFor="name"
+        >
+          Name
+        </label>
+        <div className="rounded-md relative flex  items-center shadow-sm">
+          <input
+            id="name"
+            type="email"
+            className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            required
+          />
+        </div>
+      </div>
+      <div>
+        <label
+          htmlFor="price"
+          className="mb-1 block text-sm font-medium text-gray-700"
+        >
+          Price
+        </label>
         <div className="rounded-md shadow-sm relative flex items-center">
           <div className="absolute left-0 pl-3 flex items-center justify-center pointer-events-none">
             <span className="text-gray-500 text-sm">$</span>
           </div>
-          <input id="price" className="appearance-none w-full px-3 pl-7 py-2 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500" type="text" placeholder="0.00" />
+          <input
+            id="price"
+            className="appearance-none w-full px-3 pl-7 py-2 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+            type="text"
+            placeholder="0.00"
+          />
           <div className="absolute right-0 pr-3 flex items-center pointer-events-none">
             <span className="text-gray-500 text-sm">USD</span>
           </div>
         </div>
       </div>
       <div>
-        <label className="mb-1 block text-sm font-medium text-gray-700">Description</label>
-          <textarea className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500" rows={4} />
+        <label className="mb-1 block text-sm font-medium text-gray-700">
+          Description
+        </label>
+        <textarea
+          className="mt-1 shadow-sm w-full focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
+          rows={4}
+        />
       </div>
-      <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-3 border-transparent rounded-md shadow-sm font-medium text-sm focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">Upload product</button>
+      <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-3 border-transparent rounded-md shadow-sm font-medium text-sm focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
+        Upload product
+      </button>
     </div>
   );
 };
